e4a1329eb901f42292de08c4715482530f175749
Devanshu Chatterjee <devanshu4190@gmail.com>
2023-03-28 04:27:26 +0530

    setup tailwind & removed garb files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div>
-      
-    </div>
-  )
+	return <h1 className="text-3xl font-bold underline">Hello world!</h1>
 }
 
 export default App
