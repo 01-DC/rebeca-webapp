354c0d56806f5e4244ddc5823e69c519f8094a6c
Devanshu Chatterjee <devanshu4190@gmail.com>
2023-04-05 02:23:03 +0530

    created page header component & dummy events page

--- src/components/shared/PageHeader.jsx
+++ src/components/shared/PageHeader.jsx
@@ -1,12 +1,20 @@
 import React from "react"
+import headerImage from "../../assets/header.jpg"
 
 const PageHeader = ({ title }) => {
 	return (
-		<div>
-			<div>
-				<img src="" alt="" />
+		<div className="sticky -top-28 w-screen h-64 z-30 bg-base-100">
+			<img
+				src={headerImage}
+				className="opacity-50 h-full object-cover"
+				alt="rebeca"
+			/>
+
+			<div className="absolute bottom-4">
+				<h2 className="font-bold text-4xl px-2 md:text-6xl md:px-16">
+					{title}
+				</h2>
 			</div>
-			<div>{title}</div>
 		</div>
 	)
 }
--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,7 +1,42 @@
 import React from "react"
+import PageHeader from "../components/shared/PageHeader"
+
+const events = Array.from(Array(26), (x, i) => i)
 
 const Events = () => {
-	return <div>Events</div>
+	return (
+		<div>
+			<PageHeader title={"Events & Competitions"} />
+			<div className="py-16 md:px-16">
+				<div className="flex flex-wrap justify-center gap-8">
+					{events.map((event, i) => (
+						<div
+							key={i}
+							className="card w-96 bg-base-100 shadow-xl image-full">
+							<figure>
+								<img
+									src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+									alt="Shoes"
+								/>
+							</figure>
+							<div className="card-body">
+								<h2 className="card-title">Shoes!</h2>
+								<p>
+									If a dog chews shoes whose shoes does he
+									choose?
+								</p>
+								<div className="card-actions justify-end">
+									<button className="btn btn-primary">
+										Buy Now
+									</button>
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Events
