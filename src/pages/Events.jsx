1cdccbdc8302741533e0277bc2981f664ce0c1f4
Devanshu Chatterjee <devanshu4190@gmail.com>
2023-04-05 04:07:30 +0530

    added event card component

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import PageHeader from "../components/shared/PageHeader"
+import EventCard from "../components/events/EventCard"
 
 const events = Array.from(Array(26), (x, i) => i)
 
@@ -10,28 +11,7 @@ const Events = () => {
 			<div className="py-16 md:px-16">
 				<div className="flex flex-wrap justify-center gap-8">
 					{events.map((event, i) => (
-						<div
-							key={i}
-							className="card w-96 bg-base-100 shadow-xl image-full">
-							<figure>
-								<img
-									src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-									alt="Shoes"
-								/>
-							</figure>
-							<div className="card-body">
-								<h2 className="card-title">Shoes!</h2>
-								<p>
-									If a dog chews shoes whose shoes does he
-									choose?
-								</p>
-								<div className="card-actions justify-end">
-									<button className="btn btn-primary">
-										Buy Now
-									</button>
-								</div>
-							</div>
-						</div>
+						<EventCard key={i} />
 					))}
 				</div>
 			</div>
