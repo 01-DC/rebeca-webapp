7028fd7f57674cb72759e8d78e88643a1029f6d1
Devanshu Chatterjee <devanshu4190@gmail.com>
2023-04-08 03:44:24 +0530

    updated layout of tshirt cta

--- src/components/home/TshirtCTA.jsx
+++ src/components/home/TshirtCTA.jsx
@@ -5,84 +5,52 @@ import tshirtback from "../../assets/Rebeca T shirt Back.png"
 
 const TShirtCTA = () => {
 	return (
-		<div class="w-full flex flex-col lg:flex-row py-24 gap-16 bg-base-200 px-4 lg:px-16">
-			<div class="lg:w-1/3 mx-4 flex justify-center items-center">
-				<div
-					id="carouselExampleCaptions1"
-					class="carousel slide relative carousel-fade carousel-dark drop-shadow-xl rounded-2xl border-4 border-primary my-12 w-96 py-2"
-					data-bs-ride="carousel">
-					<div class="carousel-indicators absolute bottom-4 left-0 flex flex-col gap-7 p-0 mb-8 ml-4 opacity-80">
-						<button
-							type="button"
-							data-bs-target="#carouselExampleCaptions1"
-							data-bs-slide-to="0"
-							class="active border-t-0 border-2 border-primary"
-							aria-current="true"
-							aria-label="Slide 1">
-							<img
-								src={tshirtfront}
-								class="object-cover w-9 h-9 ring-2 p-0.5 ring-base-100 rounded-md transition duration-300"
-							/>
-						</button>
-						<button
-							type="button"
-							data-bs-target="#carouselExampleCaptions1"
-							data-bs-slide-to="1"
-							aria-label="Slide 2">
-							<img
-								src={tshirtback}
-								class="object-cover w-9 h-9 ring-2 p-0.5 ring-base-100 rounded-md transition duration-300"
-							/>
-						</button>
-					</div>
-					<div class="carousel-inner relative w-full overflow-hidden ml-8">
-						<div class="carousel-item active relative float-left w-full">
-							<img
-								src={tshirtfront}
-								class="block w-full h-96 object-cover p-4"
-								alt="..."
-							/>
-						</div>
-						<div class="carousel-item relative float-left w-full">
-							<img
-								src={tshirtback}
-								class="block w-full h-96 object-cover p-4"
-								alt="..."
-							/>
-						</div>
-					</div>
-				</div>
+		<div class="bg-base-200 py-16 md:px-16">
+			<div className="relative w-full">
+				<AnimationCanvas />
+				<h2
+					className="text-4xl font-extrabold md:text-6xl pl-4 shadow-2xl absolute top-1/2 -translate-y-1/2"
+					id="about">
+					Get our official merchandise
+				</h2>
 			</div>
-			<div class="w-full px-4 flex flex-col justify-center items-center lg:items-start">
-				<div className="relative w-full">
-					<AnimationCanvas />
-					<h2
-						className="text-4xl font-extrabold md:text-6xl pl-4 shadow-2xl absolute top-1/2 -translate-y-1/2"
-						id="about">
-						Get our official merchandise
-					</h2>
+			<div className="container mx-auto">
+				<div class="flex flex-col justify-center items-center">
+					<div className="flex flex-col md:flex-row justify-between items-center gap-8 mt-16">
+						<div className="flex outline rounded-xl p-16">
+							<div class="">
+								<img src={tshirtfront} class="" alt="..." />
+							</div>
+							<div class="">
+								<img src={tshirtback} class="" alt="..." />
+							</div>
+						</div>
+						<div className="md:h-96 flex flex-col items-center justify-between gap-8">
+							<p class="text-left">
+								REBECA 2023 is back with a bang, this time with
+								a statement Tee!
+								<br />
+								It's time to gear up for the epic college fest
+								Rebeca with the sickest t-shirt design yet! This
+								limited edition t-shirt features the badass logo
+								of the event with the date and location, making
+								it a must-have for all you party animals out
+								there. Dancing to the hottest tunes this t-shirt
+								is the perfect way to show off your Rebeca
+								spirit. So, get your wallets ready and snag one
+								of these dope t-shirts before they're all gone!
+								<br />
+								#pujo_ashche #rebeca2k23
+							</p>
+							<a
+								href="https://forms.gle/S6BgU4ZaZhX7jVrL6"
+								target="__blank"
+								class="btn btn-primary btn-wide rounded-full md:ml-auto tracking-wide">
+								Get your t-shirt now
+							</a>
+						</div>
+					</div>
 				</div>
-				<p class="mt-8 text-center lg:text-start">
-					REBECA 2023 is back with a bang, this time with a statement
-					Tee!
-					<br />
-					It's time to gear up for the epic college fest Rebeca with
-					the sickest t-shirt design yet! This limited edition t-shirt
-					features the badass logo of the event with the date and
-					location, making it a must-have for all you party animals
-					out there. Dancing to the hottest tunes this t-shirt is the
-					perfect way to show off your Rebeca spirit. So, get your
-					wallets ready and snag one of these dope t-shirts before
-					they're all gone!
-					<br />
-					#pujo_ashche #rebeca2k23
-				</p>
-				<a
-					href="https://forms.gle/S6BgU4ZaZhX7jVrL6"
-					target="__blank"
-					class="btn btn-primary btn-wide rounded-full mb-4 mt-10 tracking-wide">
-					Get your t-shirt now
-				</a>
 			</div>
 		</div>
 	)
