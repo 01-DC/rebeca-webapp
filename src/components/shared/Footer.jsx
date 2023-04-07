4224689176007b5261f6381781f730b43d05470c
Devanshu Chatterjee <devanshu4190@gmail.com>
2023-04-08 02:26:09 +0530

    suggestions integrated

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -112,7 +112,9 @@ const Footer = () => {
 				<div>
 					<span className="footer-title">Socials</span>
 					<div className="flex items-center justify-center gap-4">
-						<a href="" target="_blank">
+						<a
+							href="https://www.facebook.com/rebeca.iiests"
+							target="_blank">
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
 								width="24"
@@ -122,7 +124,9 @@ const Footer = () => {
 								<path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
 							</svg>
 						</a>
-						<a href="" target="_blank">
+						<a
+							href="https://www.instagram.com/iiest.rebeca"
+							target="_blank">
 							<svg
 								width="28"
 								height="28"
