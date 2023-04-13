7c765d5cffe0cbea18f2c736bbfc6c8002b7c55c
Devanshu Chatterjee <devanshu4190@gmail.com>
2023-04-14 03:00:21 +0530

    fixed countdown text size

--- src/components/home/Countdown.jsx
+++ src/components/home/Countdown.jsx
@@ -9,30 +9,30 @@ const Countdown = () => {
 
 	return (
 		<div className="flex flex-col justify-center items-center py-16 md:px-16">
-			<div className="text-4xl font-black text-yellow-400">
+			<div className="text-5xl md:text-7xl font-black text-yellow-400">
 				#PUJO_আসছে
 			</div>
 			<div className="grid grid-flow-col gap-5 text-center auto-cols-max justify-center m-5">
 				<div className="flex flex-col">
-					<span className="countdown font-mono text-5xl">
+					<span className="countdown font-mono text-5xl md:text-7xl">
 						<span style={{ "--value": days }}></span>
 					</span>
 					days
 				</div>
 				<div className="flex flex-col">
-					<span className="countdown font-mono text-5xl">
+					<span className="countdown font-mono text-5xl md:text-7xl">
 						<span style={{ "--value": hours }}></span>
 					</span>
 					hours
 				</div>
 				<div className="flex flex-col">
-					<span className="countdown font-mono text-5xl">
+					<span className="countdown font-mono text-5xl md:text-7xl">
 						<span style={{ "--value": minutes }}></span>
 					</span>
 					min
 				</div>
 				<div className="flex flex-col">
-					<span className="countdown font-mono text-5xl">
+					<span className="countdown font-mono text-5xl md:text-7xl">
 						<span style={{ "--value": seconds }}></span>
 					</span>
 					sec
