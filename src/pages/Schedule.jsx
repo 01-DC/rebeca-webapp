84cc58a7cd1619a8b8c63e0eedb5df593faba7af
Devanshu Chatterjee <devanshu4190@gmail.com>
2023-04-19 13:41:34 +0530

    created schedule page using images

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -1,5 +1,9 @@
 import React from "react"
 import PageHeader from "../components/shared/PageHeader"
+import classical from "../assets/schedule/CLASSICAL.png"
+import beings from "../assets/schedule/BEINGS.png"
+import kolkata from "../assets/schedule/KOLKATA.png"
+import mumbai from "../assets/schedule/MUMBAI.png"
 
 const Schedule = () => {
 	return (
@@ -8,56 +12,31 @@ const Schedule = () => {
 			<div className="mx-auto py-16 lg:px-16">
 				<div className="grid grid-cols-1 xl:grid-cols-2 gap-16">
 					<div className="rounded-xl border-4 p-4 border-rebpurple">
-						<h2 className="text-4xl font-bold">
+						<h2 className="text-4xl font-bold mb-8">
 							20th April - Classical Night
 						</h2>
-						<div className="overflow-x-auto">
-							<table className="table table-zebra w-full">
-								{/* head */}
-								<thead>
-									<tr>
-										<th>Event</th>
-										<th>Time</th>
-										<th>Venue</th>
-									</tr>
-								</thead>
-								<tbody>
-									<tr>
-										<td>Cy Ganderton</td>
-										<td>Quality Control Specialist</td>
-										<td>Blue</td>
-									</tr>
-									<tr>
-										<td>Hart Hagerty</td>
-										<td>Desktop Support Technician</td>
-										<td>Purple</td>
-									</tr>
-									<tr>
-										<td>Brice Swyre</td>
-										<td>Tax Accountant</td>
-										<td>Red</td>
-									</tr>
-								</tbody>
-							</table>
-						</div>
+						<img src={classical} alt="rebeca" />
 					</div>
 
 					<div className="rounded-xl border-4 p-4 border-rebpurple">
-						<h2 className="text-4xl font-bold">
+						<h2 className="text-4xl font-bold mb-8">
 							21st April - BEings Night
 						</h2>
+						<img src={beings} alt="rebeca" />
 					</div>
 
 					<div className="rounded-xl border-4 p-4 border-rebpurple">
-						<h2 className="text-4xl font-bold">
+						<h2 className="text-4xl font-bold mb-8">
 							22nd April - Kolkata Night
 						</h2>
+						<img src={kolkata} alt="rebeca" />
 					</div>
 
 					<div className="rounded-xl border-4 border-rebpurple p-4">
-						<h2 className="text-4xl font-bold">
+						<h2 className="text-4xl font-bold mb-8">
 							23rd April - Mumbai Night
 						</h2>
+						<img src={mumbai} alt="rebeca" />
 					</div>
 				</div>
 			</div>
